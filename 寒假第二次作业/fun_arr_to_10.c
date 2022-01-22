4a12f2c28bd3de17bfa281ba393f644f57636931
deeplakee <97671080+deeplakee@users.noreply.github.com>
2022-01-22 23:48:02 +0800

    Delete 寒假第二次作业 directory
    there is something wrong.

--- 寒假第二次作业/fun_arr_to_10.c
+++
@@ -1,7 +0,0 @@
-int arr_to_10(char arr[4]) {
-	int length = strlen(arr), num = 0;
-	int i;
-	for (i = 0 ; i <= length - 1; i++)
-		num = arr[i] - 48 + num * 10;
-	return num;
-}
--- 寒假第二次作业/head.h
+++
@@ -1,6 +0,0 @@
-int arr_to_10(char arr[4]);//把一个字符串转化为一个10进制的数
-
-void num_10_to_2_8(int num,char arr[9]);//把一个10进制的数转化为一个2进制的字符串(8位)
-
-long long ipaddress_to_ipnum(char arr1[4],char arr2[4],char arr3[4],char arr4[4]);
-//把IP地址的四个数字以字符串读入并把IP地址转化为十进制表示
