277369fdb2ea7152697d0ad283cb7a3b0fbf43c0
deeplakee <97671080+deeplakee@users.noreply.github.com>
2022-01-24 11:13:30 +0800

    Delete 寒假第二次作业 directory
    有些函数用不到

--- 寒假第二次作业/fun_arr_to_10.c
+++
@@ -1,8 +0,0 @@
-#include <string.h>
-int arr_to_10(char arr[4]) {
-	int length = strlen(arr), num = 0;
-	int i;
-	for (i = 0 ; i <= length - 1; i++)
-		num = arr[i] - 48 + num * 10;
-	return num;
-}
--- 寒假第二次作业/fun_cc_to_num.c
+++
@@ -1,6 +0,0 @@
-int cc_to_num(char a1,char a2)
-{
-	int a;
-	a=a2-48+10*(a1-48);
-	return a;
-}
